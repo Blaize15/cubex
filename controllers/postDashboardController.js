630995d44753b910683f9b828f715e5b885ae012
Md Moinuddin <blaizebiebre1155@gmail.com>
2022-06-19 08:51:45 +0500

    done coding dashboard

---
+++ controllers/postDashboardController.js
@@ -0,0 +1,18 @@
+
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://broker.emqx.io')
+
+
+const postDashboardController = (req, res)=>{
+    const cubex1 = req.body.cubex1;
+    const cubex2 = req.body.cubex2;
+    const demonName = req.body.name;
+    client.publish("cubex/cubex" + cubex1 && "cubex/cubex" + cubex2 , "enable demon")
+   
+
+    res.send("Demon enabled");
+    
+}    
+
+
+module.exports = postDashboardController;
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const dashboardController = require('../controllers/dashboardController')
-
+const postDashboardController = require("../controllers/postDashboardController");
 
 
 router.get('/dashboard', dashboardController);
-
+router.post('/dashboard', postDashboardController);
 module.exports = router;
